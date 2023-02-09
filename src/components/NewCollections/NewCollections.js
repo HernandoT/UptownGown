75a388b707fb79e81d63e9c2b4bb4190abac6bb6
HernandoT <hernandotandra02@gmail.com>
2023-02-09 10:10:26 +0700

    fix new collections title

--- src/components/NewCollections/NewCollections.js
+++ src/components/NewCollections/NewCollections.js
@@ -1,12 +1,24 @@
 import "./NewCollections.css";
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
+import img from '../../utils/assets/logo.jpg'
 
 const NewCollections = () => {
+  const handleDragStart = (e) => e.preventDefault();
+
+  const items = [
+    <img src={img} onDragStart={handleDragStart} role="presentation" />,
+    <img src={img} onDragStart={handleDragStart} role="presentation" />,
+    <img src={img} onDragStart={handleDragStart} role="presentation" />,
+  ];
+
   return (
     <div className="newCollections">
       <div className="newCollectionsTitle">
         <div className="strip"></div>
         <p>New Collections</p>
       </div>
+      <AliceCarousel mouseTracking autoHeight items={items} />
 
       
     </div>
