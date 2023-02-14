93ece19709bec54905ed3b09b7958da1f6f51cc5
HernandoT <hernandotandra02@gmail.com>
2023-02-14 10:18:13 +0700

    detail

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Rent from "./pages/Rent/Rent";
 import Custom from "./pages/Custom/Custom";
 import Appointment from "./pages/Appointment/Appointment";
 import About from "./pages/About/About";
+import Detail from "./pages/Detail/Detail";
 
 function App() {
   return (
@@ -13,10 +14,11 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/rent" element={<Rent />}/>
-        <Route path="/custom" element={<Custom />}/>
-        <Route path="/appointment" element={<Appointment />}/>
-        <Route path="/about" element={<About />}/>
+        <Route path="/rent" element={<Rent />} />
+        <Route path="/custom" element={<Custom />} />
+        <Route path="/appointment" element={<Appointment />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/detail" element={<Detail />} />
       </Routes>
     </div>
   );
